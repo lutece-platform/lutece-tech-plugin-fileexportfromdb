fcea163287d76b2fb36ba8f7a1bfabeed958dc6c
isabelle-lenain <isabelle-lenain@users.noreply.github.com>
2011-06-06 16:57:28 +0000

    Update Copyright
    git-svn-id: http://dev.lutece.paris.fr/svn/lutece/portal/trunk/plugins/technical/plugin-fileexportfromdb@30596 bab10101-e421-0410-a517-8ce0973de3ef

--- src/java/fr/paris/lutece/plugins/fileexport/service/FileZipService.java
+++ src/java/fr/paris/lutece/plugins/fileexport/service/FileZipService.java
@@ -1,7 +1,7 @@
 
 
 /*
- * Copyright (c) 2002-2009, Mairie de Paris
+ * Copyright (c) 2002-2011, Mairie de Paris
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
